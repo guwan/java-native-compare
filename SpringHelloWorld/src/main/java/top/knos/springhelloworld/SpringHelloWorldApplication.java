29d7373a1529355a2487a2e24d993539d681b396
卢文武 <wu6789@qq.com>
2023-05-31 15:36:29 +0800

    修改hibernate版本

--- SpringHelloWorld/src/main/java/top/knos/springhelloworld/SpringHelloWorldApplication.java
+++ SpringHelloWorld/src/main/java/top/knos/springhelloworld/SpringHelloWorldApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringHelloWorldApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello world!");
 		SpringApplication.run(SpringHelloWorldApplication.class, args);
 	}
 
