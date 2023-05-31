e3e6900f4a12954c323eb76f20a739c49571996e
卢文武 <wu6789@qq.com>
2023-05-31 10:13:15 +0800

    first commit

---
+++ SpringHelloWorld/src/test/java/top/knos/springhelloworld/SpringHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package top.knos.springhelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ SpringWebJpa/src/test/java/top/knos/springwebjpa/SpringWebJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package top.knos.springwebjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
